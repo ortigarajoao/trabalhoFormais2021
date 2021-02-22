162534b8a28655c21146872faeed125d6b828212
Joao Ortigara <ortigara.joao@gmail.com>
2021-02-22 00:28:51 -0300

    Added initial automatoFinito class

---
+++ src/automatoFinito.py
@@ -0,0 +1,7 @@
+class automatoFinito:
+
+    def __init__(self, alfabeto, estados, estadoInicial, estadosFinais):
+        self.alfabeto = alfabeto
+        self.estados = estados
+        self.estadoInicial = estadoInicial
+        self.estadosFinais = estadosFinais
