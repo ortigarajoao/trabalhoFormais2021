7265c338b6162dc54afb6cc72e65ac23e6872ae4
Joao Ortigara <ortigara.joao@gmail.com>
2021-02-22 10:31:58 -0300

    Added initial automatoFinitoNaoDeterministico class

--- src/model/automatoFinitoNaoDeterministico.py
+++ src/model/automatoFinitoNaoDeterministico.py
@@ -1,6 +1,6 @@
-class automatoFinito:
+class automatoFinitoNaoDeterministico:
 
-    def __init__(self, alfabeto, estados, estadoInicial, estadosFinais):
+    def __init__(self, alfabeto, estados, estadoInicial, estadosFinais, epsilon):
         self.alfabeto = alfabeto
         self.estados = estados
         self.estadoInicial = estadoInicial
